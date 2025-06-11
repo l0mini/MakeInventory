3809fd70b6e8a5f5592605ae0231ecdc3e566937
l0mini <l0mini@naver.com>
2025-06-11 17:00:06 +0900

    Comment : Manager 주석추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -34,17 +34,17 @@ public class GameManager : MonoBehaviour
 
     private void SetData()
     {
-        var items = new List<ItemData>
+        var items = new List<ItemData> //아이템 리스트에 아이템 추가
         {
             new ItemData(swordIcon, 10, 5, 0, 20, ItemType.Weapon),
             new ItemData(axIcon, 20, 0, 0, 10, ItemType.Weapon),
             new ItemData(sheldIcon, 0, 15, 100, 0, ItemType.Armor),
             new ItemData(armorIcon, 0, 20, 50, 0, ItemType.Armor),
         };
-        Player = new Character("Chad", 5, 5000, items);
-        UIManager.Instance.UIMainMenu.SetCharacter(Player);
-        UIManager.Instance.UIStatus.SetCharacter(Player);
-        UIManager.Instance.UIInventory.SetCharacter(Player);
-        uiInventory.InitInventoryUI();
+        Player = new Character("Chad", 5, 5000, items);     // 플레이어 데이터 설정
+        UIManager.Instance.UIMainMenu.SetCharacter(Player); // UI 세팅
+        UIManager.Instance.UIStatus.SetCharacter(Player);   // UI 세팅
+        UIManager.Instance.UIInventory.SetCharacter(Player);// 아이템슬롯 및 UI 세팅
+        uiInventory.InitInventoryUI(); // 인벤토리 초기화
     }
 }
